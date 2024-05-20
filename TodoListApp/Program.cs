c24f69f8154f8f0420f54342a6a1eb13c8e83240
a-c-everett <andreweverett16@gmail.com>
2024-05-20 11:22:47 -0400

    Add task feture
    Added a feature to allow users to add tasks to their ToDo list. Next I will add a feature to display the list.

---
+++ TodoListApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+class TodoApp {
+
+    enum UserActions{
+        AddTask = 1,
+        Exit
+    }
+    public static void Main(string[] args) {
+        List<string> toDoList = new List<string>();
+
+        while (true) {
+            Console.WriteLine("You Can: ");
+            Console.WriteLine("1. Add a task");
+            Console.WriteLine("2. Exit");
+
+            int choice = int.Parse(Console.ReadLine());
+
+            if(choice == (int)UserActions.AddTask) {
+                Console.WriteLine("Enter the task name: ");
+                string taskName = Console.ReadLine();
+                toDoList.Add(taskName);
+                Console.Clear();
+                Console.WriteLine("Task added successfully");
+            }
+            else if(choice == (int)UserActions.Exit) {
+                break;
+            }
+        }
+    }
+}
