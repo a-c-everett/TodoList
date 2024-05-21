8f602417d7b806e8452856689b06d5f7ffb6ff6c
a-c-everett <andreweverett16@gmail.com>
2024-05-21 11:06:43 -0400

    Finished with project.

--- TodoListApp/Program.cs
+++ TodoListApp/Program.cs
@@ -1,29 +1,74 @@
 ﻿using System.Collections.Generic;
 
-class TodoApp {
+class TodoApp
+{
 
-    enum UserActions{
+    enum UserActions
+    {
         AddTask = 1,
+
+        DeleteTask,
         Exit
     }
-    public static void Main(string[] args) {
+    public static void Main(string[] args)
+    {
         List<string> toDoList = new List<string>();
 
-        while (true) {
+
+        while (true)
+        {
+            if (toDoList.Count > 0)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("ToDo List: ");
+                for (int i = 0; i < toDoList.Count; i++)
+                {
+                    Console.WriteLine("(" + (i + 1) + ") " + toDoList[i]);
+                }
+                Console.WriteLine("");
+            }
+            else
+            {
+                Console.WriteLine("");
+                Console.WriteLine("No tasks on your list.");
+                Console.WriteLine("");
+            }
+
             Console.WriteLine("You Can: ");
             Console.WriteLine("1. Add a task");
-            Console.WriteLine("2. Exit");
+            Console.WriteLine("2. Delete a task");
+            Console.WriteLine("3. Exit");
+            Console.WriteLine("");
 
             int choice = int.Parse(Console.ReadLine());
 
-            if(choice == (int)UserActions.AddTask) {
+            if (choice == (int)UserActions.AddTask)
+            {
                 Console.WriteLine("Enter the task name: ");
                 string taskName = Console.ReadLine();
                 toDoList.Add(taskName);
                 Console.Clear();
                 Console.WriteLine("Task added successfully");
             }
-            else if(choice == (int)UserActions.Exit) {
+            else if (choice == (int)UserActions.DeleteTask)
+            {
+                Console.WriteLine("Enter the number of the task you want to delete: ");
+                int taskNum = int.Parse(Console.ReadLine());
+
+                while (taskNum < 1 || taskNum > toDoList.Count)
+                {
+                    Console.WriteLine("Please enter a valid list item number: ");
+                    taskNum = int.Parse(Console.ReadLine());
+                }
+
+                toDoList.RemoveAt(taskNum - 1);
+                Console.WriteLine("Task deleted successfully!");
+
+
+
+            }
+            else if (choice == (int)UserActions.Exit)
+            {
                 break;
             }
         }
--- TodoListApp/obj/Debug/net8.0/TodoListApp.AssemblyInfo.cs
+++ TodoListApp/obj/Debug/net8.0/TodoListApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoListApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c24dd952fe235bda4a8dbc4fa201b95f0ea2833")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c24f69f8154f8f0420f54342a6a1eb13c8e83240")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoListApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoListApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
